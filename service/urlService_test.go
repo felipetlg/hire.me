778620b63f2d5baf4557e8b1d63621738593e988
Felipe Germani <lopesftl@gmail.com>
2020-10-20 01:10:38 -0300

    Flexibiliza estrutura do rep mock para facilitar criação dos testes.

--- service/urlService_test.go
+++ service/urlService_test.go
@@ -8,11 +8,15 @@ import (
 	"github.com/felipetlg/hire.me/service"
 )
 
-type RepositoryMock struct{}
+type RepositoryMock struct {
+	url  *model.Url
+	urls []model.Url
+	err  error
+}
 
 func (rm *RepositoryMock) SetupDatabase() error {
 
-	return nil
+	return rm.err
 }
 func (rm *RepositoryMock) InsertNewEntry(url *model.Url) error {
 
@@ -20,7 +24,7 @@ func (rm *RepositoryMock) InsertNewEntry(url *model.Url) error {
 }
 func (rm *RepositoryMock) RetrieveLongUrl(alias string) (*model.Url, error) {
 
-	return nil, nil
+	return rm.url, rm.err
 }
 func (rm *RepositoryMock) UpdateUrl(url *model.Url) {
 
@@ -30,15 +34,21 @@ func (rm *RepositoryMock) MostVisited(n int) ([]model.Url, error) {
 	return nil, nil
 }
 
-func TestRetrieveLongUrl(t *testing.T) {
-	var rMock repository.Repository = &RepositoryMock{}
+func TestRetrieveLongUrl_success(t *testing.T) {
+	var rMock repository.Repository = &RepositoryMock{
+		url: &model.Url{LongUrl: "test"},
+		err: nil,
+	}
 	urlService := &service.UrlService{
 		Repo: rMock,
 	}
 
-	err1, err2 := urlService.RetrieveLongUrl("alias")
+	url, err2 := urlService.RetrieveLongUrl("alias")
 
-	if err1 != nil || err2 != nil {
-		t.Errorf("Expected no error retrieving longUrl")
+	if url == nil || err2 != nil {
+		t.Fatal("Expected no error retrieving longUrl")
+	}
+	if url.LongUrl != "test" {
+		t.Fatal("LongUrl retrieved with error")
 	}
 }
